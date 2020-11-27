d93cffed8e15710ebc9e7360f727bb2981e7741e
Jonathan Fox <jf162@icloud.com>
2020-11-27 21:04:40 +0000

    Updated css and functionality.

--- lwc/resourceList/resourceList.js
+++ lwc/resourceList/resourceList.js
@@ -1,14 +1,59 @@
+import { LightningElement, wire, track } from 'lwc';
 import { publish, MessageContext } from 'lightning/messageService';
+import { getObjectInfo } from 'lightning/uiObjectInfoApi';
+import { getPicklistValues } from 'lightning/uiObjectInfoApi';
+import Military_Resource_OBJECT from '@salesforce/schema/Military_Resource__c';
+import TYPE_FIELD from '@salesforce/schema/Military_Resource__c.Type__c';
+import COUNTRY_FIELD from '@salesforce/schema/Military_Resource__c.Country__c';
 import RESOURCE_LIST_UPDATE_MESSAGE from '@salesforce/messageChannel/ResourceListUpdate__c';
 import { NavigationMixin } from 'lightning/navigation';
-import { LightningElement, wire } from 'lwc';
-/** ResourceController.searchResources(searchTerm) Apex method */
 import searchResources from '@salesforce/apex/ResourceController.searchResources';
+
 export default class ResourceList extends NavigationMixin(LightningElement) {
-	searchTerm = '';
+	
 	resources;
+	searchTerm = '';
+	searchFilterType = '';
+	@track searchFilterTypeOptions = [];
+	searchFilterCountry = '';
+	@track searchFilterCountryOptions = [];
+
+    @wire(getObjectInfo, { objectApiName: Military_Resource_OBJECT })
+    Military_ResourceInfo;
+
+    @wire(getPicklistValues,{recordTypeId: '$Military_ResourceInfo.data.defaultRecordTypeId', fieldApiName: TYPE_FIELD})
+	typeValues({error, data}){
+		if(data){
+			let mappedItems = [];
+			data.values.map(result => {
+				mappedItems.push({label: result.label, value: result.value});
+				
+			})
+			this.searchFilterTypeOptions = mappedItems;
+			
+			
+		}else if(error){
+			console.log(error);
+		}
+	}
+	@wire(getPicklistValues,{recordTypeId: '$Military_ResourceInfo.data.defaultRecordTypeId', fieldApiName: COUNTRY_FIELD})
+	countryValues({error, data}){
+		if(data){
+			let countryMappedItems = [];
+			data.values.map(result => {
+				countryMappedItems.push({label: result.label, value: result.value});
+				
+			})
+			this.searchFilterCountryOptions = countryMappedItems;
+			
+			
+		}else if(error){
+			console.log(error);
+		}
+	}
+   
 	@wire(MessageContext) messageContext;
-	@wire(searchResources, {searchTerm: '$searchTerm'})
+	@wire(searchResources, {searchTerm: '$searchTerm', searchFilterType: '$searchFilterType', searchFilterCountry: '$searchFilterCountry'})
 	loadResources(result) {
 		this.resources = result;
 		if (result.data) {
@@ -29,9 +74,41 @@ export default class ResourceList extends NavigationMixin(LightningElement) {
 			this.searchTerm = searchTerm;
 		}, 300);
 	}
+
+	handleTypeChange(event) {
+		// Debouncing this method: do not update the reactive property as
+		// long as this function is being called within a delay of 300 ms.
+		// This is to avoid a very large number of Apex method calls.
+		window.clearTimeout(this.delayTimeout);
+		const searchFilterType = event.target.value;
+		// eslint-disable-next-line @lwc/lwc/no-async-operation
+		this.delayTimeout = setTimeout(() => {
+			this.searchFilterType = searchFilterType;
+		}, 300);
+	}
+	clearFilters(){
+		this.searchFilterType = '';
+		this.searchTerm = '';
+		this.searchFilterCountry = '';
+	}
+	
+	handleCountryChange(event) {
+		// Debouncing this method: do not update the reactive property as
+		// long as this function is being called within a delay of 300 ms.
+		// This is to avoid a very large number of Apex method calls.
+		window.clearTimeout(this.delayTimeout);
+		const searchFilterCountry = event.target.value;
+		// eslint-disable-next-line @lwc/lwc/no-async-operation
+		this.delayTimeout = setTimeout(() => {
+			this.searchFilterCountry = searchFilterCountry;
+		}, 300);
+	}
+	
+
 	get hasResults() {
 		return (this.resources.data.length > 0);
 	}
+
 	handleResourceView(event) {
 		// Get resource record id from resourceview event
 		const resourceId = event.detail;
--- lwc/resourceLocation/resourceLocation.js
+++ lwc/resourceLocation/resourceLocation.js
@@ -1,13 +1,16 @@
 import { LightningElement, api, wire } from 'lwc';
 import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 // Set Resource object fields
 const NAME_FIELD = 'Military_Resource__c.Name';
 const LOCATION_LATITUDE_FIELD = 'Military_Resource__c.Location__Latitude__s';
 const LOCATION_LONGITUDE_FIELD = 'Military_Resource__c.Location__Longitude__s';
+const TYPE_FIELD = 'Military_Resource__c.Type__c';
 const resourceFields = [
 	NAME_FIELD,
 	LOCATION_LATITUDE_FIELD,
-	LOCATION_LONGITUDE_FIELD
+  LOCATION_LONGITUDE_FIELD,
+  TYPE_FIELD
 ];
 export default class ResourceLocation extends LightningElement {
   @api recordId;
@@ -16,17 +19,24 @@ export default class ResourceLocation extends LightningElement {
   @wire(getRecord, { recordId: '$recordId', fields: resourceFields })
   loadResource({ error, data }) {
     if (error) {
-      // TODO: handle error
+      this.dispatchEvent(
+        new ShowToastEvent({
+            title: 'Error Reciving Map Location',
+            message: 'Error reciving map marker - ' + error.body.message,
+            variant: 'error',
+        }),
+    );
     } else if (data) {
       // Get Resource data
       this.name =  getFieldValue(data, NAME_FIELD);
       const Latitude = getFieldValue(data, LOCATION_LATITUDE_FIELD);
       const Longitude = getFieldValue(data, LOCATION_LONGITUDE_FIELD);
+      const Type = getFieldValue(data, TYPE_FIELD);
       // Transform resource data into map markers
       this.mapMarkers = [{
         location: { Latitude, Longitude },
         title: this.name,
-        description: `Type: ${resource.Type__c}`,
+        description: Type
       }];
     }
   }
